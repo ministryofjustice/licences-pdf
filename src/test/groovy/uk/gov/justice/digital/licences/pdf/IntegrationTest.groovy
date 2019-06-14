6a6f958cff231f6c101e7eb9887db07aed645533
Peter Phillips <peter@greenthistle.co.uk>
2019-06-14 11:38:07 +0100

    DT-48: Improve build.gradle and add endpoint to return application/pdf

--- src/test/groovy/uk/gov/justice/digital/licences/pdf/IntegrationTest.groovy
+++ src/test/groovy/uk/gov/justice/digital/licences/pdf/IntegrationTest.groovy
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.licences.pdf
 
 import groovyx.net.http.RESTClient
+import org.apache.commons.io.IOUtils
 
 import static groovyx.net.http.ContentType.JSON
 
@@ -18,5 +19,23 @@ class IntegrationTest extends TestSpecification {
         result.status == 200
         result.data.subList(0, 5) == [37, 80, 68, 70, 45]
         result.data.size > 1000
+        result.headers['Content-Type'].value == 'application/json;charset=UTF-8'
+    }
+
+    def "POST generate creates a PDF and returns as an application/pdf"() {
+
+        when:
+        def result = new RESTClient(getBaseUrl()).post(
+                path: 'generate-pdf',
+                requestContentType: JSON,
+                body: [templateName: 'template', values: [ABC: 'ABC1234D']]
+        )
+
+        then:
+        result.status == 200
+        byte[] buffer = IOUtils.toByteArray(result.data)
+        buffer[0..4] == [37, 80, 68, 70, 45]
+        buffer.length > 1000
+        result.headers['Content-Type'].value == 'application/pdf'
     }
 }
--- src/test/groovy/uk/gov/justice/digital/licences/pdf/PdfGeneratorTest.groovy
+++ src/test/groovy/uk/gov/justice/digital/licences/pdf/PdfGeneratorTest.groovy
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.licences.pdf
 
-
+import org.apache.commons.io.IOUtils
 import spock.lang.Specification
 import uk.gov.justice.digital.licences.pdf.data.PdfRequest
 import uk.gov.justice.digital.licences.pdf.service.PdfGenerator
@@ -9,6 +9,20 @@ import uk.gov.justice.digital.licences.pdf.service.TemplateRepository
 
 class PdfGeneratorTest extends Specification {
 
+    def "PdfGenerator processes a PdfRequest and creates a PDF from a template (as ints)"() {
+
+        setup:
+        def pdfGenerator = new PdfGenerator(new TemplateEngine(new TemplateRepository()))
+        def pdfRequest = new PdfRequest('template', [ABC: 'ABC1234D'])
+
+        when:
+        def result = pdfGenerator.generate(pdfRequest).body
+
+        then:
+        result[0..5] == [37, 80, 68, 70, 45, 49].collect { it.byteValue() }
+        result.length > 1000
+    }
+
     def "PdfGenerator processes a PdfRequest and creates a PDF from a template"() {
 
         setup:
@@ -16,7 +30,7 @@ class PdfGeneratorTest extends Specification {
         def pdfRequest = new PdfRequest('template', [ABC: 'ABC1234D'])
 
         when:
-        def result = pdfGenerator.process(pdfRequest).body
+        def result = pdfGenerator.generatePdf(pdfRequest).body
 
         then:
         result[0..5] == [37, 80, 68, 70, 45, 49].collect { it.byteValue() }
